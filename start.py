3ba42666f570ed244c0fd4e200222d892b48dbbf
Gabriel Claudiu Georgiu <gabriel@claudiugeorgiu.com>
2020-04-10 21:33:40 +0200

    Format code with Black

--- start.py
+++ start.py
@@ -1,16 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
+#!/usr/bin/env python3
 
 import logging
 
 from adb.adb import ADB
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # Logging configuration.
     logger = logging.getLogger(__name__)
-    logging.basicConfig(format='%(asctime)s> [%(levelname)s][%(name)s][%(funcName)s()] %(message)s',
-                        datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s> [%(levelname)s][%(name)s][%(funcName)s()] %(message)s",
+        datefmt="%d/%m/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
 
     # This is an example file showing how the adb wrapper can be used.
 
@@ -21,13 +23,15 @@ if __name__ == '__main__':
     adb.connect()
 
     adb_version = adb.get_version()
-    logger.info('ADB version: {0}'.format(adb_version))
+    logger.info("ADB version: {0}".format(adb_version))
 
     connected_devices = adb.get_available_devices()
-    logger.info('Connected devices: {0}'.format(connected_devices))
+    logger.info("Connected devices: {0}".format(connected_devices))
 
     # Set the first device in the list as the target of the subsequent commands.
     adb.target_device = connected_devices[0]
 
     adb.wait_for_device()
-    logger.info('Message from Android device: {0}'.format(adb.shell(['echo "Hello World!"'])))
+    logger.info(
+        "Message from Android device: {0}".format(adb.shell(['echo "Hello World!"']))
+    )
